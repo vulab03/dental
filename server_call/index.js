5f82f6db9d7b67f77d654ed464ade80e4001a93c
tun2g <tunghcltt@gmail.com>
2023-09-07 21:49:23 +0700

    add server call

---
+++ server_call/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+
+require("dotenv").config();
+
+
+const app = express()
+
+app.use("/",(req,res)=>{
+    return res.json({"message":"Hello"})
+})
+
+app.use((req,res,next)=>{
+    next(createError(404,"Not Found"))
+})
+
+
+PORT = process.env.PORT 
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://127.0.0.1:${PORT}`)
+})
